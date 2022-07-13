f269b3e10406f7c33e148ad5379eb9af3ede055f
Vakhtang Tivadze <vaxotivadze@gmail.com>
2022-07-13 13:22:25 +0400

    Apply custom React hook useCounter

--- src/hooks/use-counter.js
+++ src/hooks/use-counter.js
@@ -1,15 +1,17 @@
 import { useState, useEffect } from 'react';
 
-const useCounter = () => {
+const useCounter = (step) => {
   const [counter, setCounter] = useState(0);
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setCounter((prevCounter) => prevCounter + 1);
+        setCounter((prevCounter) => prevCounter + step);
     }, 1000);
 
     return () => clearInterval(interval);
-  }, []);
+  }, [step]);
+
+  return counter;
 };
 
 export default useCounter;
